47f1d1f8c6d51ce16039cece96ae4f8a872d3691
Rostyslav Sokalskyi <rostyslav.sokalskyi.oi.2022@lpnu.ua>
2024-09-12 12:25:51 +0300

    feat: add ui components; update: styles file

---
+++ portfolio/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Link from "next/link";
+import { Button } from "./ui/Button";
+import Nav from "./Nav";
+
+export default function Header() {
+  return (
+    <header className="py-12 xl:py-12 text-white ">
+      <div className="container mx-auto flex items-center justify-between">
+        {/* Logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Rostyslav<span className="text-accent">.</span>
+          </h1>
+        </Link>
+
+        {/**desktop nav and hire button */}
+        <div className="hiden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="contact">
+            <Button className="ml-auto">Hire me</Button>
+          </Link>
+        </div>
+
+        {/* <div className="xl:hidden">mobile nav</div> */}
+      </div>
+    </header>
+  );
+}
